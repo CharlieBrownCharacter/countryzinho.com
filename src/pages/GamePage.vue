892b07d2c79a7cead4beeebe1e8e9544e0a8d2fb
Bruno Francisco <17223411+CharlieBrownCharacter@users.noreply.github.com>
2025-07-20 20:47:06 +0200

    - removed unused guessed drawer

--- src/pages/GamePage.vue
+++ src/pages/GamePage.vue
@@ -2,7 +2,6 @@
 import CountrySVG from '@/components/CountrySVG.vue'
 import StartGameModal from '@/components/StartGameModal.vue'
 import ConfirmGameRestartDialog from '@/components/ConfirmGameRestartDialog.vue'
-import GuessedCountriesDrawer from '@/components/GuessedCountriesDrawer/GuessedCountriesDrawer.vue'
 import GuessInput from '@/components/GuessInput.vue'
 import ResultsDialog from '@/components/ResultsDialog.vue'
 import { onMounted } from 'vue'
@@ -19,7 +18,6 @@ onMounted(() => {
   <main class="relative w-full p-0 m-0 box-content">
     <GuessInput />
     <ResultsDialog />
-    <GuessedCountriesDrawer />
     <CountrySVG />
     <StartGameModal />
     <ConfirmGameRestartDialog />
