01c66543f242f99237d5d062bce0263f7ed535a8
Bruno Francisco <17223411+CharlieBrownCharacter@users.noreply.github.com>
2025-07-21 22:23:52 +0200

    - translated `GuessInput` component

--- src/components/GuessInput.vue
+++ src/components/GuessInput.vue
@@ -5,10 +5,14 @@ import { findGuess } from '@/services/resources/country/helpers.ts'
 import InputText from 'primevue/inputtext'
 import { usePostHog } from '@/composables/usePostHog.ts'
 import TimerSection from '@/components/TimerSection.vue'
+import { useI18n } from 'vue-i18n'
+import type { MessageSchema } from '@/services/i18n'
 
 const guess = ref('')
 
 const duplicatedGuessedCountry = ref('')
+
+const { t } = useI18n<{ message: MessageSchema }>()
 
 const countryStore = useCountryStore()
 
@@ -64,13 +68,15 @@ function onKeyUp() {
       class="p-4 rounded-lg bg-(--p-inputtext-background) absolute top-5 left-1/2 -translate-x-1/2 w-64 px-4 py-2 shadow border border-(--p-inputtext-border-color) has-[input:focus]:border-(--p-inputtext-focus-border-color) transition-colors"
       :class="[duplicatedGuessedCountry ? '!border-red-500' : '']"
     >
-      <label for="country-guess" class="sr-only">Guess the country</label>
+      <label for="country-guess" class="sr-only">
+        {{ t('components.guess-input.type-country-name') }}
+      </label>
       <InputText
         id="country-guess"
         ref="inputRef"
         class="border-none px-0"
         v-model="guess"
-        placeholder="Type a country name..."
+        :placeholder="t('components.guess-input.type-country-name')"
         autofocus
         autocomplete="off"
         data-op-ignore
@@ -81,7 +87,7 @@ function onKeyUp() {
         class="text-xs text-red-500"
         :class="[duplicatedGuessedCountry ? 'visible' : 'invisible']"
       >
-        {{ duplicatedGuessedCountry }} already entered
+        {{ t('components.guess-input.country-duplicated', { country: duplicatedGuessedCountry }) }}
       </div>
 
       <TimerSection />
