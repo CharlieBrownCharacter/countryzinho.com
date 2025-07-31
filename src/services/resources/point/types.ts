bc689c9ecf8db1afc444cf8441d31e56fb4320ae
Bruno Francisco <17223411+CharlieBrownCharacter@users.noreply.github.com>
2025-07-31 18:36:35 +0200

    - added tests for `pointsStore`

--- src/services/resources/point/types.ts
+++ src/services/resources/point/types.ts
@@ -1,6 +1,14 @@
 import type { Continent } from '@/services/resources/country/types.ts'
 
-export type PointHistory =
-  | { type: 'guessedCountry'; points: number; createdAt: Date }
-  | { type: 'fastGuess'; points: number; createdAt: Date }
-  | { type: 'guessedWholeContinent'; points: number; createdAt: Date; continent: Continent }
+export type PointGuessedCountry = { type: 'guessedCountry'; points: number; createdAt: Date }
+
+export type PointFastGuess = { type: 'fastGuess'; points: number; createdAt: Date }
+
+export type PointGuessedContinent = {
+  type: 'guessedWholeContinent'
+  points: number
+  createdAt: Date
+  continent: Continent
+}
+
+export type PointHistory = PointGuessedCountry | PointFastGuess | PointGuessedContinent
